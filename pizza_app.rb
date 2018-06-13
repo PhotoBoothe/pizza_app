bdf85440070e89e5fb786975aa24ecfb54e7a63a
Boothe Davis <photoboothe@yahoo.com>
2018-06-13 16:00:32 -0400

    pizza orders

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,26 +1,32 @@
 def build_pizza()
-	meat = meats().sample
-	cheese = cheese().sample
-	vegetables = vegetables().sample
-	crust = crust().sample
-	size = size().sample
+	puts "How many pizzas would you like?" 
+	pizza_count = gets.chomp.to_i
+	pizza_count.times do
+		meat = meat().sample
+		cheese = cheese().sample
+		vegetables = vegetables().sample
+		crust = crust().sample
+		size = size().sample
+		puts "This is your #{size}, #{crust} pizza with #{meat}, #{cheese}, and #{vegetables}."
+	end
 end
 
-def meats()
-	meats = ["pepperoni", "sausage","ham", "bacon", "hamburger"]
+def meat()
+	meat = ["pepperoni", "sausage","ham", "bacon", "hamburger"]
 end
 
 def cheese()
 	cheese = ["mozzarella", "romano", "cheddar"]
 end
 
-def vegetables
+def vegetables()
 	vegetables = ["onions","mushrooms", "green peppers"]
 end
 def crust()
-	crust = ["thin", "deep", "stuffed"]
+	crust = ["thin", "deep dish", "stuffed crust"]
 end
 
 def size()
 	size = ["small", "medium", "large"]
 end
+puts build_pizza()
