63b8a5dd2ec1801c82d8a6129b1f492f42787b73
Boothe Davis <photoboothe@yahoo.com>
2018-06-19 15:20:24 -0400

    added detailed subtotals

--- pizza_app.rb
+++ pizza_app.rb
@@ -4,29 +4,32 @@ def build_pizza()
 	# puts pizza_count.class
 	price = 0
 	price = pizza_count * 10
+	puts "First price is $#{price}."
 	pizza_count.times do
-	meat = meat().sample
-	if meat == "pepperoni"
-		price = price + 2
-	elsif meat == "sausage"
-		price = price + 2
-	elsif meat == "ham"
-		price = price + 2
-	elsif meat == "bacon"
-		price = price + 2
-	elsif meat == "hamburger"
-		price = price + 2
-	end
+		meat = meat().sample
+		if meat == "pepperoni"
+			price = price + 2
+		elsif meat == "sausage"
+			price = price + 2
+		elsif meat == "ham"
+			price = price + 2
+		elsif meat == "bacon"
+			price = price + 2
+		elsif meat == "hamburger"
+			price = price + 2
+		end
+		puts "After meat your subtotal is $#{price}."
 	# puts meat
 	# p "Meat sample is #{meat}" 
-	cheese = cheese().sample
-	if cheese == "mozzarella"
-		price = price + 2
-	elsif cheese == "cheddar"
-		price = price + 2
-	elsif cheese == "romano"
-		price = price + 2
-	end
+		cheese = cheese().sample
+		if cheese == "mozzarella"
+			price = price + 2
+		elsif cheese == "cheddar"
+			price = price + 2
+		elsif cheese == "romano"
+			price = price + 2
+		end
+		puts "After cheese your subtotal is $#{price}."
 	# puts cheese
 	
 	# p "Cheese sampla is #{cheese}"
